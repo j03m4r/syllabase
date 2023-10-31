e2c4705aceb1a6a169add1e4ee94233e8aa2f83e
j03m4r <joeemarr12345@gmail.com>
2023-10-31 14:54:00 -0500

    Dynamically grabbing classes with supabase queries. Added authentication. Updated upload syllabus modal to associate uploaded syllabi with the logged in user. Added profile avatars and update profile functionality.

--- app/(site)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-	<div className="flex w-full h-full justify-center items-center text-6xl font-bold">
-		asdf
-	</div>
-  )
-}
